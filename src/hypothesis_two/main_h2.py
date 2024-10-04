a4c45fa53d2e03bf9f069b638b333c8ff7a22ad3
Derik <dekribeiro7@gmail.com>
2024-10-04 02:53:24 -0300

    evitando conflitos

--- src/hypothesis_two/main_h2.py
+++ src/hypothesis_two/main_h2.py
@@ -2,7 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import categorizer as ctg
 import cleaner as cln
-import graphic as gp
+import ploter as ptr
 
 df = pd.read_csv("../../data/Crime_Data_from_2020_to_Present.csv")
 df = df[['DATE OCC', 'Crm Cd']]
@@ -10,5 +10,5 @@ df = df[['DATE OCC', 'Crm Cd']]
 df = cln.date_to_year(df, 'DATE OCC')
 df = ctg.add_category(df)
 
-gp.save_png(df, 'DATE OCC', 'category',  '../../data/pandemic/')
-gp.save_png(df, 'category', 'DATE OCC', '../../data/pandemic/')
+ptr.save_png(df, 'DATE OCC', 'category',  '../../data/pandemic/')
+ptr.save_png(df, 'category', 'DATE OCC', '../../data/pandemic/')
